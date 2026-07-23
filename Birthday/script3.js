760f18e4489fb42121dfb9b6cc4e1ff939cb4a6f
RadaroCode <tankun.dee51@gmail.com>
2026-07-23 22:41:48 +0700

    Delete Birthday directory

--- Birthday/script3.js
+++
@@ -1,7 +0,0 @@
-function showLetter() {
-    document.getElementById("letter").classList.toggle("show");
-}
-
-function showCake() {
-    document.getElementById("cakeModal").classList.toggle("show");
-}
