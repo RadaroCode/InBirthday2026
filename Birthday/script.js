b6c1883fda49184bf3f3eb75bc7f465688022118
RadaroCode <tankun.dee51@gmail.com>
2026-07-23 22:19:29 +0700

    Add files via upload

---
+++ Birthday/script.js
@@ -0,0 +1,26 @@
+let current = 0;
+
+const dialogues = [
+    "สวัสดีค้าบบบบ",
+    "อ๋าา นี่คือสุดสวยของจื๋อนี่เอง",
+    "ก็นะต้องขอโชว์บางอย่างก่อนที่จะเข้าเรื่อง",
+    "ลองมาทบทวนกันดีกว่าา ว่าตั้งแต่เราคุยกันมามีอะไรเกิดขึ้นบ้าง"
+];
+
+const pictures = [
+    "images/hi.jpg",
+    "images/radaro4.png",
+    "images/radaro1.png",
+    "images/radaro2.png"
+];
+
+function nextDialogue() {
+    current++;
+
+    if (current < dialogues.length) {
+        document.getElementById("dialogue").textContent = dialogues[current];
+        document.getElementById("photo").src = pictures[current];
+    } else {
+        window.location.href = "page2.html";
+    }
+}
